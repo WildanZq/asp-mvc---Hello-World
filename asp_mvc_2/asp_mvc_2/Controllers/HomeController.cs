e0ca070146c17be044b764b5d233b1166341c9a2
Wildan <wildan2wildan@gmail.com>
2017-02-20 10:02:01 +0700

    Modul1 done

---
+++ asp_mvc_2/asp_mvc_2/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace asp_mvc_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
