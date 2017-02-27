3fe1d7a58ec33e19fc83909868f8ef219b89f0fd
Wildan <wildan2wildan@gmail.com>
2017-02-27 08:23:48 +0700

    Modul3 done

--- asp_mvc_2/asp_mvc_2/Models/ViewModel/UserModel.cs
+++ asp_mvc_2/asp_mvc_2/Models/ViewModel/UserModel.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace asp_mvc_2.Models.ViewModel
 {
     public class UserModel
     {
-
+        
     }
 }
